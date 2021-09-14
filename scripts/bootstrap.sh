fe340215d3277c1426b2925c28ea97341faef6eb
L1MeN9Yu <baal998.mophisto@gmail.com>
2021-09-14 22:57:08 +0800

    bootstrap

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,4 +2,6 @@
 
 set -e
 
-./swift-project/swift/utils/update-checkout --tag swift-5.4.3-RELEASE --clone-with-ssh
+git clone https://github.com/apple/swift.git swift-project/swift
+
+./swift-project/swift/utils/update-checkout --tag swift-5.4.3-RELEASE --clone
